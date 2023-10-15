47a79b2298117ed6882fe00e47c4c3911c43c2c7
Mark Makilan <mark.makilan@gets.ph>
2023-10-15 20:40:00 +0800

    added try catch in db transact

--- app/Livewire/Public/Components/Modals/Steps.php
+++ app/Livewire/Public/Components/Modals/Steps.php
@@ -21,49 +21,55 @@ class Steps extends Component
 
     public function submit() 
     {
-        DB::transaction(function () {
-            $transaction = Transaction::create([
-                'package_id' => $this->package->id,
-                'package_amount' => $this->package->price,
-                'addons_total_amount'=> collect(data_get($this->addons, '*.*'))->sum('amount'),
-                'customize_total_amount' => collect(data_get($this->customize, '*.*'))->sum('amount'),
-                'addons' => (object) $this->addons,
-                'customize' => (object) $this->customize,
-                'remarks' => null
-            ]);
-
-            Reservation::create([
-                'transaction_id' => $transaction->id,
-                'name' => $this->reservation['name'],
-                'contact' => $this->reservation['contact'],
-                'date_of_use' => $this->reservation['date_of_use'],
-                'location' => $this->reservation['location'],
-                'email' => $this->reservation['email'],
-            ]);
-
-            Payment::create([
-                'transaction_id' => $transaction->id,
-                'name' => $this->payment['name'],
-                'type' => 'down_payment',
-                'amount' => $this->payment['amount'],
-                'ref_no' => $this->payment['ref_no'],
-                'email' => $this->payment['email']
-            ]);
-
-            $this->js('alert("Your reservation has been submitted.")');
-
-            return redirect()->route('home');
-        });
+        try {
+            DB::transaction(function () {
+                $transaction = Transaction::create([
+                    'package_id' => $this->package->id,
+                    'package_amount' => $this->package->price,
+                    'addons_total_amount'=> collect(data_get($this->addons, '*.*'))->sum('amount'),
+                    'customize_total_amount' => collect(data_get($this->customize, '*.*'))->sum('amount'),
+                    'addons' => (object) $this->addons,
+                    'customize' => (object) $this->customize,
+                    'remarks' => null
+                ]);
+    
+                Reservation::create([
+                    'transaction_id' => $transaction->id,
+                    'name' => $this->reservation['name'],
+                    'contact' => $this->reservation['contact'],
+                    'date_of_use' => $this->reservation['date_of_use'],
+                    'location' => $this->reservation['location'],
+                    'email' => $this->reservation['email'],
+                ]);
+    
+                Payment::create([
+                    'transaction_id' => $transaction->id,
+                    'name' => $this->payment['name'],
+                    'type' => 'down_payment',
+                    'amount' => $this->payment['amount'],
+                    'ref_no' => $this->payment['ref_no'],
+                    'email' => $this->payment['email']
+                ]);
+    
+                $this->js('alert("Your reservation has been submitted.")');
+    
+                return redirect()->back();
+            });
+        } catch (\Throwable $th) {
+            $this->js('alert("We\'re unable to process your reservation at this time.")');
+        }
     }
 
     public function increment($type, $keys) {
         $key = explode('.', $keys);
+        
         $this->$type[$key[0]][$key[1]][$key[2]]++;
         $this->$type[$key[0]][$key[1]]['amount'] = $this->$type[$key[0]][$key[1]][$key[2]] * $this->$type[$key[0]][$key[1]]['price'];
     }
 
     public function decrement($type, $keys) {
         $key = explode('.', $keys);
+
         $this->$type[$key[0]][$key[1]][$key[2]] != 0 ? $this->$type[$key[0]][$key[1]][$key[2]]-- : 0;
         $this->$type[$key[0]][$key[1]]['amount'] = $this->$type[$key[0]][$key[1]][$key[2]] * $this->$type[$key[0]][$key[1]]['price'];
     }
