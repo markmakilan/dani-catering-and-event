0605fbd7ea3a4b231cdf054bbce49e03d89c5f92
Mark Makilan <mark.makilan@gets.ph>
2023-10-16 17:36:09 +0800

    added soft deletes

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Package extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    protected $dates = ['deleted_at'];
     
     protected $casts = [
         'addons' => 'object',
