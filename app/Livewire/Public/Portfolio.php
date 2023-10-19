b49952cc209d11ca5db8a763199a4d15a49886e8
Mark Makilan <mark.makilan@gets.ph>
2023-10-19 21:22:15 +0800

    dynamic portfolio

--- app/Livewire/Public/Portfolio.php
+++ app/Livewire/Public/Portfolio.php
@@ -4,8 +4,20 @@ namespace App\Livewire\Public;
 
 use Livewire\Component;
 
+use App\Models\Gallery;
+
 class Portfolio extends Component
 {
+    public $images = [];
+
+    public function mount() {
+        $this->images = $this->gallery()->get();
+    }
+    
+    public function gallery() {
+        return Gallery::where('type', 'portfolio');
+    }
+
     public function render()
     {
         return view('livewire.public.portfolio')->extends('layouts.public.app');
