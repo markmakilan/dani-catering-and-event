65d7f3d9c7b8034513d7184d7d2ab2631a171e61
Mark Makilan <mark.makilan@gets.ph>
2023-10-21 20:12:06 +0800

    carousel

--- app/Livewire/Public/Index.php
+++ app/Livewire/Public/Index.php
@@ -4,9 +4,31 @@ namespace App\Livewire\Public;
 
 use Livewire\Component;
 
+use App\Models\Gallery;
+
 class Index extends Component
 {
     public $current = 0;
+
+    public $images = [];
+
+    public function mount() {
+        $this->images = $this->gallery()->get();
+    }
+
+    public function toggleNext()
+    {
+        $this->current = ($this->current + 1) % count($this->images);
+    }
+
+    public function togglePrevious()
+    {
+        $this->current = ($this->current - 1 + count($this->images)) % count($this->images);
+    }
+    
+    public function gallery() {
+        return Gallery::where('type', 'carousel');
+    }
 
     public function render()
     {
--- resources/views/livewire/public/index.blade.php
+++ resources/views/livewire/public/index.blade.php
@@ -4,49 +4,31 @@
             <div class="overflow-hidden rounded-2xl">
                 <div class="flex transition-transform" style="transform: translateX(-{{ $current }} * 100%);">
                     <div class="flex-shrink-0">
-                        <img 
-                            src="{{ asset('assets/img/slider.png') }}" 
-                            class="w-screen h-screen object-cover"
-                            alt="carousel-image"
-                            x-show="currentSlide == 0">
-                        <img 
-                            src="https://img.freepik.com/premium-photo/restaurant-lunch-catering-buffet-with-different-dishes_219193-3429.jpg" 
-                            class="w-screen h-screen object-cover"
-                            alt="carousel-image"
-                            x-show="currentSlide == 1">
-                        <img 
-                            src="https://media.istockphoto.com/id/650655146/photo/catering-food-wedding-event-table.jpg?s=612x612&w=0&k=20&c=ATGYgW8bM_559jJ5aUNO4HlJqpkOWUmNNMMflx5kajo=" 
-                            class="w-screen h-screen object-cover"
-                            alt="carousel-image"
-                            x-show="currentSlide == 2">
+                        @forelse ($images as $key => $image)
+                        
+                            @if ($current == $key)
+                            <img 
+                                src="{{ asset($image->getFirstMedia('gallery')->getUrl()) }}" 
+                                class="w-screen h-screen object-cover"
+                                alt="carousel-image">
+                            @endif
+                        @empty
+                            
+                        @endforelse
                     </div>
                 </div>
             </div>
             
+            @if ($images->count() > 1)
             <div class="absolute h-full top-0 left-0 right-0 flex justify-between">
-                <button x-on:click="togglePrevious" class="w-20 flex items-center justify-center">
+                <button wire:click="togglePrevious" class="w-20 flex items-center justify-center">
                     <x-icons.caret-left class="w-14 h-14" fill="#f8e046" />
                 </button>
-                <button x-on:click="toggleNext" class="w-20 flex items-center justify-center">
+                <button wire:click="toggleNext" class="w-20 flex items-center justify-center">
                     <x-icons.caret-right class="w-14 h-14" fill="#f8e046" />
                 </button>
             </div>
+            @endif
         </div>
     </div>
-
-    <script>
-        document.addEventListener('alpine:init', () => {
-            Alpine.data('carousel', () => ({
-                currentSlide: @entangle('current'),
-     
-                toggleNext() {
-                    this.currentSlide = (this.currentSlide + 1) % 3
-                },
-
-                togglePrevious() {
-                    this.currentSlide = (this.currentSlide - 1 + 3) % 3
-                },
-            }))
-        })
-    </script>
 </div>
